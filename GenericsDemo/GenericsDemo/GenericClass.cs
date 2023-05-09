93796921431f70a408c07e2d481f6ebfd63763ca
“DeoreAnkitaRahul” <ankitapatil375@gmail.com>
2023-05-09 18:47:16 +0530

    Add : Generic Practice

---
+++ GenericsDemo/GenericsDemo/GenericClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsDemo
+{
+    public class GenericClass<T>
+    {
+        public T data;
+
+        public GenericClass(T data)
+        {
+            this.data = data;
+            Console.WriteLine("Data :" +this.data);
+        }
+         public void DemoData(T data)
+        {
+            Console.WriteLine("Data : " + data);
+        }
+
+        public void DemoData<T>(T data)
+        {
+            Console.WriteLine("Data : " + data);
+        }
+
+
+
+
+
+
+
+
+    }
+}
---
+++ GenericsDemo/GenericsDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace GenericsDemo
+{
+    class Program
+    {
+       
+        //generics means general form and do not have return
+        public static void Main(string[] args)
+        {
+            GenericClass<string> genericClass= new GenericClass<string>("A");
+            GenericClass<int> genericCls= new GenericClass<int>(3);
+            GenericClass<double> generic = new GenericClass<double>(3.3);
+
+            genericClass.DemoData("Am");
+             genericCls.DemoData(2);
+            generic.DemoData(5.5);
+
+            genericCls.DemoData(6);
+            generic.DemoData(4.5);
+
+
+
+        }
+    }
+}
